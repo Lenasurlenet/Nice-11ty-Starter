e2042b64665cc78b772144f57f81ad2bcae65ab6
Lenasurlenet <48878136+Lenasurlenet@users.noreply.github.com>
2021-12-28 11:49:32 +0100

    Directions in eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,17 @@
 module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addWatchTarget("./src/_includes/");
+  eleventyConfig.addWatchTarget('./src/images/');
+  eleventyConfig.addWatchTarget('./src/icons/');
+  eleventyConfig.addWatchTarget('./src/css/tailwind.config.js');
   eleventyConfig.addWatchTarget('./src/css/tailwind.css');
+
+  eleventyConfig.addPassthroughCopy('./src/_includes/');
+  eleventyConfig.addPassthroughCopy('./src/images/');
+  eleventyConfig.addPassthroughCopy('./src/icons/');
+
   return {
-    markdownTemplateEngine: "njk",
-    templateFormats: ["html", "njk", "md"],
+    
     dir: {
       input: 'src',
       output: '_site'
